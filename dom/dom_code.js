228e789af3ac0d7343edda90b31eee9493c6489e
PonomarenkoNina <ninulka@ukr.net>
2024-01-17 15:27:21 +0200

    added exersise and new files

---
+++ dom/dom_code.js
@@ -0,0 +1,6 @@
+var access = document.getElementById("code9");
+var code = access.innerHTML;
+code = code + "midnight";
+alert(code);
+// <p id="code9">My watch stopped at</p>
+// Console.log: My watch stopped at midnight
