ba2643d8c1bd9d2296c392410cf084a23f0df812
PonomarenkoNina <ninulka@ukr.net>
2024-01-22 00:35:01 +0200

    added files

---
+++ game/game.js
@@ -0,0 +1,5 @@
+// Definition of variables - Визначення змінних
+
+// Calling a function and creating a loop - виклик функції та створення циклу, перевірка результатів
+
+// Output of game results - вивід результатів гри
